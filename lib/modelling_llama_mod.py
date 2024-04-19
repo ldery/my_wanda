f318ddb1fc14111ee8d4d1e12aa6c175ad3222e0
ldery <derylucio@gmail.com>
2024-04-19 14:39:29 -0400

    .

--- lib/modelling_llama_mod.py
+++ lib/modelling_llama_mod.py
@@ -249,7 +249,7 @@ class LlamaAttention(nn.Module):
 		elif self.prune_method == "wanda":
 			with torch.no_grad():
 				ins_ = attn_output.reshape(bsz, q_len, self.hidden_size).reshape(-1, self.hidden_size).to(torch.float32)
-				ins_ = self.o_proj.weight.data.abs() * (ins_.pow(2).mean(0, keepdim=True).sqrt()).to(torch.float16)
+				ins_ = (self.o_proj.weight.data.to(torch.float32).abs()) * (ins_.pow(2).mean(0, keepdim=True).sqrt())
 				self.intermed_cache = ins_.mean(axis=0).view(1, 1, self.num_heads, -1).mean(axis=-1, keepdim=True)
 
 				if self.intermed_cache.isinf().any() or self.intermed_cache.isnan().any():
@@ -318,7 +318,7 @@ class LlamaMLP(nn.Module):
 				self.intermed_cache = intermed_result.abs().view(-1, last_dim).mean(axis=0, keepdims=True).view(1, 1, -1)
 			elif self.prune_method == "wanda":
 				ins_ = intermed_result.view(-1, last_dim).to(torch.float32)
-				ins_ = self.down_proj.weight.data.abs() * (ins_.pow(2).mean(0, keepdim=True).sqrt()).to(torch.float16)
+				ins_ = (self.down_proj.weight.data.to(torch.float32).abs()) * (ins_.pow(2).mean(0, keepdim=True).sqrt())
 				self.intermed_cache = ins_.mean(axis=0).view(1, 1, -1)
 				if self.intermed_cache.isinf().any() or self.intermed_cache.isnan().any():
 					print("We hit a nan or inf. Stopping")
